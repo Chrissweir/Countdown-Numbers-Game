cfef3c9469e30d4c4a83f289ec512459ad2b3a0b
Christopher Weir <christopher.weir@hotmail.com>
2017-02-07 15:09:34 +0000

    Defined the list of Numbers
    Defined a list called nums which contains the set of numbers that will
    be chosen at random.

--- Countdown.rkt
+++ Countdown.rkt
@@ -1,6 +1,11 @@
 #lang racket
 
-(define (Calculate x (list)))
+;Define a list called nums which contains the set of numbers that will be chosen at random.
+(define nums(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+
+
+(define (calculate x list)
+  (+ (car list) x))
 
 
 (define (generate min max)
@@ -8,4 +13,4 @@
       0
     (cons min (generate (+ min 1) max))))
 
-(generate 101 999)
+(calculate 5 nums)
