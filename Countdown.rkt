7287e0f336c8d2b9bbca9b7e4272e7756d16872e
Christopher Weir <christopher.weir@hotmail.com>
2017-02-19 17:12:41 +0000

    Changed comments, added custom operators, added references
    Changes some of the comments as I changed some of the code. I added
    custom operators to cut down the amount of calculations the program will
    have to check. However these are not final nor am I using them right
    now! I also added references at the bottom of the Coundown.rkt file.

--- Countdown.rkt
+++ Countdown.rkt
@@ -12,33 +12,35 @@
 ;Define custom operators
 (define operators'(?+ ?- ?* ?/))
 
-;Define a function (select-numbers) that will take in a list, then get a random value from that list.
-;It will then remove that value from the givin list, and then add that value to the list (selected-numbers).
-;The function will then check if the list n is equal to 6, meaning there are 6 numbers in the
-;list. If this is so then the function will end, if not then it will repeat with the modified
-;list as the input.
-(define (select-numbers l)
-  (define r(list-ref l (random (length l))))
-  (set! l(remove r l))
-  (set! selected-numbers(cons r selected-numbers))
-  (if(= (length selected-numbers) 6)
-     selected-numbers
-  (select-numbers l)))
+;Define a function (select-numbers) that will take in a list, then using a for loop[1], get a random[2] value
+;from that list using list-ref[3]. It will then remove[4] that value from the givin list, and then add that value
+;to the list (selected-numbers) using set![5]. The for loop is set to loop 6 times allowing for 6 numbers to be assigned to
+;the list(selected-numbers).
+(define (select-numbers numbers)
+(for([i 6])
+  (define random-number(list-ref numbers (random (length numbers))))
+  (set! numbers(remove random-number numbers))
+  (set! selected-numbers(cons random-number selected-numbers))))
 
 ;Define a function (get-target-number) that takes in two values (min, max) which will calculate a random
-;number between the min and max.
+;number between the min and max. Max will be 1000 in this case for there to be a possibility for 999 to be selected.
 (define (get-target-number min max)
   (set! target-number(+ (random min max) target-number)) target-number)
 
 ;Main Function Calculate
-(define (calculate x list)
-  (+ (car list) x))
+(define (calculate x list operators)
+  (display "Countdown Number Game Solver\n============================\n")
+  (display(string-append "Numbers to choose from: " (~v nums) "\n"))
+  (select-numbers nums)
+  (display(string-append "Numbers selected: " (~v selected-numbers) "\n"))
+  (display(string-append "Target number: " (~v (get-target-number 101 1000)) "\n" )));Min = 101, Max - 1000
 
-(get-target-number 101 1000);Max has to be 1000 in this case for there to be a possibility for 999 to be selected.
+
+;Call the Main Function
+(calculate target-number selected-numbers operators)
 
 
-(select-numbers nums)
-
+;Custom operators - NOT FINALIZED
 (define (?+ a b)
   (cond
     [(null? a) 0] 
@@ -47,4 +49,39 @@
     [(zero? a) 0]
     [else (+ a b)]))
 
-(?+ 0 1)
+(define (?- a b)
+  (cond
+    [(null? a) 0] 
+    [(null? b) 0] 
+    [(< a b) 0]
+    [(zero? b) 0]
+    [else (- a b)]))
+
+(define (?* a b)
+  (cond
+    [(null? a) 0] 
+    [(null? b) 0] 
+    [(< a b) 0]
+    [(= a 1) 0]
+    [(= b 1) 0]
+    [(= a 0) 0]
+    [(= b 0) 0]
+    [else (* a b)]))
+
+(define (?/ a b)
+  (cond
+    [(null? a) 0] 
+    [(null? b) 0] 
+    [(< a b) 0]
+    [(zero? b) 0]
+    [(= b 1) 0]
+    [(inexact? (/ a b)) 0]
+    [else (/ a b)]))
+
+;REFERENCES
+;================================
+;[1]: for (https://docs.racket-lang.org/guide/for.html).
+;[2]: random (https://docs.racket-lang.org/reference/pairs.html#%28def._%28%28lib._racket%2Fprivate%2Flist..rkt%29._remove%29%29).
+;[3]: list-ref (https://docs.racket-lang.org/reference/pairs.html#%28def._%28%28quote._~23~25kernel%29._list-ref%29%29).
+;[4]: remove (https://docs.racket-lang.org/reference/pairs.html#%28def._%28%28lib._racket%2Fprivate%2Flist..rkt%29._remove%29%29).
+;[5]: set! (http://docs.racket-lang.org/reference/set_.html).
