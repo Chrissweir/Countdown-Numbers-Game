8484ba3031724e2dab617f30afab45245f36009c
Christopher Weir <christopher.weir@hotmail.com>
2017-02-28 13:34:17 +0000

    Solving for two numbers
    Currently solving for two numbers, I have set the target-number to 10
    for the current test purposes. This program outputs all the equations
    and if the answer is correct then a success message is displayed after
    the equation.

--- Countdown.rkt
+++ Countdown.rkt
@@ -4,6 +4,9 @@
 ;Module: Theory of Algorithms
 ;Project: Countdown Numbers Game Solver
 
+;Define the namespace
+(define ns (make-base-namespace))
+
 ;Define a list called nums which contains the set of numbers that will be chosen at random.
 (define nums(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 
@@ -11,45 +14,23 @@
 (define selected-numbers(list))
 
 ;Define a variable called target-number that will store the value of the target number.
-(define target-number 15)
+(define target-number 10)
 
 ;Define custom operators
-(define operators '(?+ ?- ?/ ?*))
+(define operators (list "+" "-" "/" "*"))
 
-(define ns (make-base-namespace))
-
-
-;Define a function that adds two numbers
-(define (cal l)
-  (display(string-append "(+" (~v (car l)) " " (~v (cadr l)) ") = "(~v(+(car l)(cadr l)))"\n"))
-  (display(string-append "(-" (~v (car l)) " " (~v (cadr l)) ") = "(~v(-(car l)(cadr l)))"\n"))
-  (display(string-append "(/" (~v (car l)) " " (~v (cadr l)) ") = "(~v(/(car l)(cadr l)))"\n"))
-  (display(string-append "(*" (~v (car l)) " " (~v (cadr l)) ") = "(~v(*(car l)(cadr l)))"\n"))
-  )
-
-(define (?+ l)
-  (display(string-append "(+" (~v (car l)) " " (~v (cadr l)) ") = "(~v(+(car l)(cadr l)))"\n"))
-  (+(car l)(cadr l)))
-
-(define (?- l)
-  (display(string-append "(-" (~v (car l)) " " (~v (cadr l)) ") = "(~v(-(car l)(cadr l)))"\n")))
-
-(define (?/ l)
-  (display(string-append "(/" (~v (car l)) " " (~v (cadr l)) ") = "(~v(/(car l)(cadr l)))"\n")))
-
-(define (?* l)
-  (display(string-append "(*" (~v (car l)) " " (~v (cadr l)) ") = "(~v(*(car l)(cadr l)))"\n")))
-
+(define (createEquation op n m)
+  (string-append "(" op " " (~v n) " " (~v m) ")"))
 
 ;Define a function (select-numbers) that will take in a list, then using a for loop[1], get a random[2] value
 ;from that list using list-ref[3]. It will then remove[4] that value from the givin list, and then add that value
 ;to the list (selected-numbers) using set![5]. The for loop is set to loop 6 times allowing for 6 numbers to be assigned to
 ;the list(selected-numbers).
 (define (select-numbers numbers)
-(for([i 2])
-  (define random-number(list-ref numbers (random (length numbers))))
-  (set! numbers(remove random-number numbers))
-  (set! selected-numbers(cons random-number selected-numbers))))
+  (for([i 2])
+    (define random-number(list-ref numbers (random (length numbers))))
+    (set! numbers(remove random-number numbers))
+    (set! selected-numbers(cons random-number selected-numbers))))
 
 ;Define a function (get-target-number) that takes in two values (min, max) which will calculate a random
 ;number between the min and max. Max will be 1000 in this case for there to be a possibility for 999 to be selected.
@@ -62,32 +43,27 @@
   (display(string-append "Numbers to choose from: " (~v nums) "\n"))
   (select-numbers nums)
   (display(string-append "Numbers selected: " (~v selected-numbers) "\n"))
-  ;(display(string-append "Target number: " (~v (get-target-number 101 1000)) "\n" ));Min = 101, Max - 1000
+ ; (display(string-append "Target number: " (~v (get-target-number 101 1000)) "\n" ));Min = 101, Max - 1000
 
-
-
-
-  ;(define sets(combinations selected-numbers 2))
+  ;Get all permutations of the sets
   (define sets(permutations selected-numbers))
+
+  ;For every set in the sets
   (for([set sets])
+    ;Display the set
     (display(string-append "\nSet: " (~v set) "\n"))
+    (define num1 (car set))
+    (define num2 (car (cdr set)))
     (for([o operators])
-      (define test(?+ set))
-      (if(= test target-number)
-         (display (cons o set))
-          0
-          )
-                  
-            ;(define mmm(string-append (~v(cons o selected-numbers))))
-    ;(define test(eval (read (open-input-string (string-append mmm))) ns))
-     ; (display o))
-    ;(for([n set])
-     ; (display(string-append "Number: " (~v n) "\n")))
-  ;(map cal sets)
-  ;(cartesian-product operators sets
-      )))
-
-
+      (define currentEquation(createEquation o num1 num2))
+      (define currentAnswer (eval (read (open-input-string currentEquation)) ns))
+      ;Display the calculation
+      (display (string-append (~v currentEquation) " = "(~v currentAnswer)))
+      ;Evaluate if answer is equal to the answer number
+      (if ( = currentAnswer target-number)
+          (display (string-append  " --> success" "\n" )) (display (string-append "\n" ))) ;TODO return)
+      )
+    ))
   
 ;Call the Main Function
 (calculate target-number operators)
