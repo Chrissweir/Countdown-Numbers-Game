83ede46b3e4d52c448733d34a6e8f17cb33fe56c
Christopher Weir <christopher.weir@hotmail.com>
2017-04-18 19:46:28 +0100

    Updated layout of file and removed some duplicates

--- Countdown.rkt
+++ Countdown.rkt
@@ -14,20 +14,25 @@
 (define selected-numbers(list))
 
 ;Define a variable called target-number that will store the value of the target number.
-(define target-number 10)
+(define target-number 0)
 
 ;Define custom operators
 (define operators (list "+" "-" "/" "*"))
 
-(define (createEquation op n m)
-  (string-append "(" op " " (~v n) " " (~v m) ")"))
+(define permCount 0)
+(define correctCount 0)
+
+;Define a function to construct the equation
+(define (createEq oper a b)
+  (~a "( " oper " "   a  " "  b " )")
+  )
 
 ;Define a function (select-numbers) that will take in a list, then using a for loop[1], get a random[2] value
 ;from that list using list-ref[3]. It will then remove[4] that value from the givin list, and then add that value
 ;to the list (selected-numbers) using set![5]. The for loop is set to loop 6 times allowing for 6 numbers to be assigned to
 ;the list(selected-numbers).
 (define (select-numbers numbers)
-  (for([i 2])
+  (for([i 6])
     (define random-number(list-ref numbers (random (length numbers))))
     (set! numbers(remove random-number numbers))
     (set! selected-numbers(cons random-number selected-numbers))))
@@ -38,74 +43,70 @@
   (set! target-number(+ (random min max) target-number)) target-number)
 
 ;Main Function Calculate
-(define (calculate x operators)
+(define (menu)
+  (select-numbers nums)
   (display "Countdown Number Game Solver\n============================\n")
   (display(string-append "Numbers to choose from: " (~v nums) "\n"))
-  (select-numbers nums)
   (display(string-append "Numbers selected: " (~v selected-numbers) "\n"))
- ; (display(string-append "Target number: " (~v (get-target-number 101 1000)) "\n" ));Min = 101, Max - 1000
+  (display(string-append "Target Number: " (~v(get-target-number 101 1000))"\n"));Min = 101, Max - 1000))))
+  (display "\nCalculating.....\n")
+  (define sets(remove-duplicates(permutations selected-numbers)))
+  (for ([set sets])
+    (define firstValue (car set))
+    (set! set (remove firstValue set))
+    (calculate set firstValue)
+    )
+  (display "==========================================================================")
+  )
+  
 
-  ;Get all permutations of the sets
-  (define sets(permutations selected-numbers))
+;Initial method for beginning the search
+(define(calculate selected-numbers oAnswer )
+  ;Outer for loop iterating through each number in list
+  (for([currentNumber selected-numbers])
+    ;Remove the used number from the current list
+    (set! selected-numbers (remove currentNumber selected-numbers))
+    ;Inner for loop applying each sign to the equation
+    (for ([sign operators])
+        
+      ;Define the current equation
+      (define currentEq (createEquation oAnswer sign currentNumber));
+      ;Calculate the current Equation
+      (define currentAnswer (eval (read (open-input-string currentEq)) ns))
 
-  ;For every set in the sets
-  (for([set sets])
-    ;Display the set
-    (display(string-append "\nSet: " (~v set) "\n"))
-    (define num1 (car set))
-    (define num2 (car (cdr set)))
-    (for([o operators])
-      (define currentEquation(createEquation o num1 num2))
-      (define currentAnswer (eval (read (open-input-string currentEquation)) ns))
-      ;Display the calculation
-      (display (string-append (~v currentEquation) " = "(~v currentAnswer)))
       ;Evaluate if answer is equal to the answer number
       (if ( = currentAnswer target-number)
-          (display (string-append  " --> success" "\n" )) (display (string-append "\n" ))) ;TODO return)
+          (begin
+            ;(display (string-append  " success" "\n" ))
+            (display (~a  currentEq " = " currentAnswer "\n") )
+            (set! permCount (+ permCount 1))
+            (set! correctCount (+ correctCount 1)))
+            
+          (begin
+            ;(display (string-append (~v currentEq) " = "(~v currentAnswer) "\n") )
+            (set! permCount (+ permCount 1))
+            )) ;TODO return)
+        
+      ;Evaluate if the answer is a negative number or a fraction
+      (if (exact-positive-integer? currentAnswer )
+          (begin
+            ;Call the function again 
+            (calculate selected-numbers currentEq))
+            
+          (display ""))
       )
-    ))
-  
-;Call the Main Function
-(calculate target-number operators)
+    )
+  )
 
-;Custom operators - NOT FINALIZED
-;(define (?+ a b)
-;  (cond
-;    [(null? a) 0] 
-;    [(null? b) 0] 
-;    [(< a b) 0]
-;    [(zero? a) 0]
-;    [else (+ a b)]))
-;
-;(define (?- a b)
-;  (cond
-;    [(null? a) 0] 
-;    [(null? b) 0] 
-;    [(< a b) 0]
-;    [(zero? b) 0]
-;    [else (- a b)]))
-;
-;(define (?* a b)
-;  (cond
-;    [(null? a) 0] 
-;    [(null? b) 0] 
-;    [(< a b) 0]
-;    [(= a 1) 0]
-;    [(= b 1) 0]
-;    [(= a 0) 0]
-;    [(= b 0) 0]
-;    [else (* a b)]))
+;Method for calculating the current equation
+(define (createEquation oAnswer sign currentNumber)
+  (~a "( " sign " "   oAnswer  " "  (~v currentNumber) " )")
+  )
 
-;(define (?/ a b)
-;  (cond
-;    [(null? a) 0] 
-;    [(null? b) 0] 
-;    [(< a b) 0]
-;    [(zero? b) 0]
-;    [(= b 1) 0]
-;    [(inexact? (/ a b)) 0]
-;    [else (/ a b)]))
-
+;Call the main search method
+(menu)
+(display (~a "\nTotal Permutations: " permCount "\n"))
+(display (~a "Correct Permutations: " correctCount ))
 
 ;REFERENCES
 ;================================
